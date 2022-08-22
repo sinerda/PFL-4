08f1491f3ea3828504fb4451079de86bfd7ad61e
Andrey Sinyavskiy <sinerda@gmail.com>
2022-08-22 12:13:48 +0300

    adaptive pt.3 + header pt.2

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
-// $('.header-slider').slick({
-//   dots: true,
-//   arrows: false
-// })
+$(function () {
+  $('.menu__btn-open').on('click', function () {
+    $('.menu__main').removeClass('menu__main--close');
+  })
+  $('.menu__btn-close').on('click', function () {
+    $('.menu__main').addClass('menu__main--close');
+  })
+
+  $('.header-slider').slick({
+    dots: true,
+    arrows: false,
+    // autoplay: true,
+    autoplaySpeed: 5000
+  });
+})
